dd9003dccca1b58f039e3ac5a8c37fd8c657770d
Elias Silva <eliasmanuelsilva@hotmail.com>
2017-06-02 14:15:34 +0100

    NewsLoader and QueryUtils implemented

--- app/src/main/java/com/example/android/newsapp/MainActivity.java
+++ app/src/main/java/com/example/android/newsapp/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.android.newsapp;
 
+import android.app.LoaderManager;
+import android.content.Loader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<News>> {
 
+    private static final int NEWS_LOADER_ID = 1;
     private NewsAdapter mAdapter;
 
     @Override
@@ -16,13 +20,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ArrayList<News> newsList = new ArrayList<>();
-        newsList.add(new News("Politics", "Theresa May rules out participating in TV debates before election", "2017-04-18", "17:20"));
-        newsList.add(new News("Politics", "Theresa May rules out participating in TV debates before election", "2017-04-18", "17:20"));
 
         ListView newsListView = (ListView) findViewById(R.id.list);
         mAdapter = new NewsAdapter(this, newsList);
 
         newsListView.setAdapter(mAdapter);
 
+        LoaderManager loaderManager = getLoaderManager();
+        loaderManager.initLoader(NEWS_LOADER_ID, null, this);
     }
+
+    @Override
+    public Loader<List<News>> onCreateLoader(int id, Bundle args) {
+        return new NewsLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> news) {
+        mAdapter.clear();
+        if (news != null && !news.isEmpty()) {
+            mAdapter.addAll(news);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+        mAdapter.clear();
+    }
+
 }
